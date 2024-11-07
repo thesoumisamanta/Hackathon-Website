9f286116958eee6d7307c338327a397033b310d4
thesoumisamanta <samantasoumi10@gmail.com>
2024-11-07 09:03:10 -0500

    create user and hackathon model

---
+++ backend/src/models/hackathon.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const hackathonSchema = mongoose.Schema({
+    image: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        index: true,
+        required: true,
+    },
+    decription: {
+        type: String,
+        required: true
+    },
+    place: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: String,
+        required: true
+    },
+    endDate: {
+        type: String,
+        required: true
+    },
+    prizeAmount: {
+        type: String,
+        required: true
+    },
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    maxParticipants: {
+        type: Number,
+        default: 5
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+hackathonSchema.plugin(mongooseAggregatePaginate)
+
+export const Hackathon = mongoose.model("Hackathon", hackathonSchema)
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcryptjs from "bcryptjs"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"]
+        unique: true,
+        trim: true,
+        minlength: 6,
+        maxlength: 12,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+        minlength: 8,
+        maxlength: 16
+    },
+    role: {
+        type: String,
+        default: "participant",
+        enum: ["participant", "developer"]
+    },
+    participatedHackathons: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hackathon"
+    }],
+    refreshToken: {
+        type: String
+    }
+}, { timesptamps: true })
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+
+    this.password = await bcryptjs.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    const isMatch = await bcryptjs.compare(password, this.password)
+    return isMatch
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        { 
+            _id: this._id ,
+            email: this.email,
+            username: this.username
+        }, 
+        process.env.ACCESS_TOKEN_SECRET, 
+        { 
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+             _id: this._id 
+        }, 
+        process.env.REFRESH_TOKEN_SECRET, 
+        { 
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY 
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
